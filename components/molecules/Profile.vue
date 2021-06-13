6ff9805d184101ab0b9f99c22c912ca632ca3231
Tiago Carioca <tiagoacarioca@hotmail.com>
2021-06-13 15:20:51 -0400

    feat: profile and xp-bar components

---
+++ components/molecules/Profile.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="flex items-center space-x-6">
+		<img src="https://github.com/tiagokriok.png" alt="Tiago Carioca (Gripen.Dev)" class="w-20 h-20 rounded-full">
+		<div class="flex flex-col w-full">
+			<strong class="text-title text-2xl">Tiago Carioca (Gripen.Dev)</strong>
+			<p class="flex items-center space-x-2">
+				<img src="icons/level.svg" alt="level">
+				<span class="text-base">Level {{ level }}</span>
+			</p>
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { mapState } from 'vuex';
+
+export default Vue.extend({
+	computed: mapState('Challenges', ['level']),
+});
+</script>
