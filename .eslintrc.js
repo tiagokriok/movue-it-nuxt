8978e30796243f7110816b16a5f503d0599d729d
Tiago Carioca <tiagoacarioca@hotmail.com>
2021-06-13 16:35:07 -0400

    feat: countdown component

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,26 +4,20 @@ module.exports = {
 		browser: true,
 		node: true,
 	},
-	extends: [
-		'@nuxtjs/eslint-config-typescript',
-		'plugin:nuxt/recommended',
-	],
-	plugins: [
-	],
+	extends: ['@nuxtjs/eslint-config-typescript', 'plugin:nuxt/recommended'],
+	plugins: [],
 	// add your custom rules here
 	rules: {
 		'comma-dangle': ['error', 'always-multiline'],
 		'import/first': 'off',
 		'import/order': 'off',
 		indent: 'off',
-		'@typescript-eslint/indent': [
-			'error',
-			'tab',
-		],
+		'@typescript-eslint/indent': ['error', 'tab'],
 		'no-new': 'off',
 		'no-tabs': 'off',
 		semi: ['error', 'always'],
 		'vue/html-indent': ['error', 'tab'],
 		'vue/order-in-components': 'off',
+		'space-before-function-paren': 'error',
 	},
 };
